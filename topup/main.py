9961caf1706ded6d4887d7f5f3ebf87f77b77b4b
kwobia-at-303179111604 <kwobia@303179111604>
2021-04-06 17:27:07 +0000

    update

--- topup/main.py
+++ topup/main.py
@@ -5,6 +5,12 @@ from api.routes import user, authentication, vodafone, glo
 
 app = FastAPI()
 
+app = FastAPI(
+    title="E-Top Up API ",
+    description="This is an EtopUp API designed to make airtime top up much easier and convenient",
+    version="1.0.0",
+)
+
 models.Base.metadata.create_all(engine)
 
 app.include_router(user.router)
